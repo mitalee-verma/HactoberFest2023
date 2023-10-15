dbc52917d2478a7baefbfc10c0027552f4086311
Mitalee Verma <85283048+mitalee-verma@users.noreply.github.com>
2023-10-15 15:39:24 +0530

    Create QRgenerator.py
    geerater QR code for any link

---
+++ QRgenerator.py
@@ -0,0 +1,25 @@
+import qrcode
+
+# Text or data you want to encode in the QR code
+data = "Hello, World!"
+
+# Create a QR code object
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Add data to the QR code
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image to a file
+img.save("my_qr_code.png")
+
+# To display the image (requires PIL)
+# img.show()
